66d441a8d17ca0d70e43f5885d983ade27752f37
Douglas Yuan <59560618+dougyuan@users.noreply.github.com>
2022-08-24 04:31:30 -0700

    Done

--- tree.h
+++ tree.h
@@ -1,12 +1,13 @@
 // tree.h
-
 #ifndef METRICTREE_H
 #define METRICTREE_H
 
 typedef struct MetricTree Tree;
 
+// MetricTree draws inspiration from Steve Hanov's
+// implementation of a partition tree. Citation in the report.
 struct MetricTree{
-  // Array of size d containing the point's coordinates
+  // Starting (vantage) point
   double* vp;
   // Median distance of point from all other points
   double md;
@@ -23,36 +24,36 @@ struct MetricTree{
     Input: X Input data points, stored as [n-by-d] array
     Input: n Number of data points (rows of X)
     Input: d Number of dimensions (columns of X)
-    Return: The vantage-point tree
+    Return: The tree
 */
-Tree* buildvp(double *X, int n, int d);
+Tree* build(double *X, int n, int d);
 // Return vantage-point subtree with points inside radius
 /*
-    Input: node A vantage-point tree
-    Return: The vantage-point subtree
+    Input: A metric tree node
+    Return: The subtree
 */
 Tree* getInner(Tree* T);
 // Return vantage-point subtree with points outside radius
 /*
-    Input: node A vantage-point tree
-    Return: The vantage-point subtree
+    Input: A metric tree node
+    Return: The subtree
 */
 Tree* getOuter(Tree* T);
 // Return median of distances to vantage point
 /*
-    Input: node A vantage-point tree
-    Return: The median distance
+    Input: A metric tree node
+    Return: The subtree
 */
 double getMD(Tree* T);
 // Return the coordinates of the vantage point
 /*
-    Input: node A vantage-point tree
+    Input: A metric tree node
     Return: The coordinates [d-dimensional vector]
 */
 double * getVP(Tree* T);
-// Return the index of the vantage point
+// Return the index of the starting point
 /*
-    Input: node A vantage-point tree
+    Input: A metric tree node
     Return: The index to the input vector of data points
 */
 int getIDX(Tree* T);
